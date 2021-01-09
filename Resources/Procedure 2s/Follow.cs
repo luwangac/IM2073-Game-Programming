5cb77792e826af274c148e72f86b967cdfb25057
Christopher Luwanga <ccl441@nyu.edu>
2021-01-09 19:17:27 +0800

    Update Follow.cs

--- Resources/Procedure 2s/Follow.cs
+++ Resources/Procedure 2s/Follow.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +6,6 @@ public class Follow : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform target;
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
